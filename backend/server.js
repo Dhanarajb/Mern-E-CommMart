5b10d8565e39a5155e94e577485f00f9f6408df6
Dhanaraj <dhanarajbhaskar690@gmail.com>
2022-12-12 12:23:37 +0530

    viewpage

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ app.get("/api/products", (req, resp) => {
   resp.send(data.products);
 });
 
+app.get("/api/products/slug/:slug", (req, resp) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    resp.send(product);
+  } else {
+    resp.status(404).send("product not found");
+  }
+  resp.send(data.products);
+});
+
 const port = process.env.PORT || 5200;
 app.listen(port, () => {
   console.log(`Server connected on ${port}`);
